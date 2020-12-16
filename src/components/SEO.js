a3437144362752052c55da0149c631eafdb42477
Phansaphong Saiuswan <615159090009@dpu.ac.th>
2020-12-16 23:24:44 +0700

    Facebook card
    Facebook card change

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import { graphql, useStaticQuery } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 const getData = graphql`
   query {
@@ -16,19 +16,36 @@ const getData = graphql`
     }
   }
 `
+
 const SEO = ({ title, description }) => {
   const { site } = useStaticQuery(getData)
-  const { siteDesc, siteTitle, siteUrl, image, twitterUser } = site.siteMetadata
+
+  const {
+    siteDesc,
+    siteTitle,
+    siteUrl,
+    image,
+    twitterUser,
+  } = site.siteMetadata
+
   return (
-    <Helmet htmlAttributes={{ lang: "th" }} title={`${title} | ${siteTitle}`}>
-      <meta name="descrition" content={description || siteDesc} />
+    <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
+      <meta name="description" content={description || siteDesc} />
       <meta name="image" content={image} />
-      {/* twiitter card */}
+      {/* facebook cards */}
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={siteTitle} />
+      <meta property="og:description" content={siteDesc} />
+      <meta property="og:image" content={`${siteUrl}${image}`} />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="300" />
+      {/* twitter card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitterUser} />
       <meta name="twitter:title" content={siteTitle} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image " content={`${siteUrl}${image}`} />
+      <meta name="twitter:description" content={siteDesc} />
+      <meta name="twitter:image" content={`${siteUrl}${image}`} />
     </Helmet>
   )
 }
